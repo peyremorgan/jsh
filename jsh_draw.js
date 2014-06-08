073a42f816eb5dcdc42b2e6eea79283e4dc15bab
morgan <morgan.peyre@etu.isae-ensma.fr>
2014-06-08 17:36:30 +0000

    added keyboard controls for arrow movement

--- jsh_draw.js
+++ jsh_draw.js
@@ -1,12 +1,55 @@
-// Create a circle shaped path with its center at the center
-// of the view and a radius of 30:
-var center = view.center;
+// Prevent right-click menu
+document.body.oncontextmenu = function(){return false;}
+
 var sides = 3;
 var radius = 7;
-var triangle = new Path.RegularPolygon(center, sides, radius);
-triangle.fillColor = '#1BBDCD';
+var center = view.center;
+var arrow = new Path.RegularPolygon(center, sides, radius);
+arrow.fillColor = '#1BBDCD';
+
+var rotateSpeed = 3;
+var rotateDirection = 0;
 
 function onResize(event) {
-	// Whenever the window is resized, recenter the path:
-	triangle.position = view.center;
+  // Whenevr the window is resized, recenter the path:
+  arrow.position = view.center;
+  arrow.translate(new Point(0,view.center.y/-5));
+}
+
+function onFrame(event) {
+  arrow.rotate(rotateSpeed*rotateDirection, view.center);
+}
+
+function onKeyDown(event) {
+	switch(event.key)
+    {
+      case 'left':
+  		event.preventDefault();
+        rotateDirection = -1;
+        break;
+        
+      case 'right':
+  		event.preventDefault();
+        rotateDirection = 1;
+        break;
+        
+        //Fixme : won't handle both arrows pressed
+    }
+}
+
+function onKeyUp(event) {
+  
+  switch(event.key)
+    {
+      case 'left':
+        rotateDirection = 0;
+        break;
+        
+      case 'right':
+        rotateDirection = 0;
+        break;
+        
+        //Fixme : won't handle both arrows pressed
+    }
+  
 }
