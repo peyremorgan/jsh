5d0dbaf21d460d9b618dec2be2025ddf99ef02c9
lapinoux02 <paulbnt@free.fr>
2014-06-08 17:47:03 +0100

    je me passe de commentaire

--- jsh_draw.js
+++ jsh_draw.js
@@ -1,8 +1,8 @@
 // Create a circle shaped path with its center at the center
 // of the view and a radius of 30:
-var center = new Point(50, 50);
+var center = view.center;
 var sides = 3;
-var radius = 40;
+var radius = 7;
 var triangle = new Path.RegularPolygon(center, sides, radius);
 triangle.fillColor = '#1BBDCD';
 
